536491c872999b87358014d1a0a01b735771a5ba
GitDmitry11 <lebeatauivre@ya.ru>
2021-09-10 19:33:16 +0300

    hfh

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -13,7 +13,7 @@ class Main extends React.Component {
 
     componentDidMount() {
         console.log(process.env);
-        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
+        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=godfather`)
             .then((response) => response.json())
             .then((data) =>
                 this.setState({ movies: data.Search, loading: false })
