07c0c4f07512e8094a3c172315d328b0b5f9d88c
GitDmitry11 <lebeatauivre@ya.ru>
2021-09-10 19:10:03 +0300

    volta

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,14 +2,14 @@ function Header() {
   return (
     <nav className="grey darken-3">
       <div className="nav-wrapper">
-        <a href="#" className="brand-logo" center>
-          Your Favorite Movie
+        <a href="https://gitdmitry11.github.io/react_movies/" className="brand-logo" >
+          Your Movies
         </a>
-        {/* <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
-            <a href="#">Repo</a>
-          </li> */}
-        {/* </ul> */}
+            <a href="https://github.com/GitDmitry11/react_movies">Repo</a>
+          </li> 
+        </ul>
       </div>
     </nav>
   );
