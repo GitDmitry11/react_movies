ecbae5130d2a24fefad6765cb2065d5ae6f81e6e
GitDmitry11 <lebeatauivre@ya.ru>
2021-09-10 11:00:19 +0300

    xyu

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer () {
+    return  <footer className="page-footer grey darken-3">
+      <div className="footer-copyright">
+        <div className="container">
+        © 2021 Dmitry Gashkov 
+        </div>
+      </div>
+    </footer>
+}
+
+export {Footer}
